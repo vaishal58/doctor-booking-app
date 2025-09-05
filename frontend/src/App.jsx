fbbd88478a1ea6907bd80202f786e3d90364cff9
Vaishal shah <vaishal0508@gmail.com>
2025-09-05 17:20:38 -0400

    route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-blue-500"></h1>
+    <div className="mx-4 sm:max-[10%]">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/doctors" element={<Doctors></Doctors>}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors></Doctors>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path="/my-profile" element={<MyProfile></MyProfile>}></Route>
+        <Route path='/my-appointments' element={<MyAppointments></MyAppointments>}></Route>
+        <Route path="/appointment/:docId" element={<Appointment></Appointment>}></Route>
+      </Routes>
     </div>
   );
 };
