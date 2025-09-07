bbd6eb87b11f1dc3a4a9d87d2afe9054dcd401b7
Vaishal shah <vaishal0508@gmail.com>
2025-09-06 20:12:21 -0400

    navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
+  const navigate=useNavigate();
+  const [showMenu,setshowMenu]=useState(false);
+  const [token,setToken]=useState(true)
+
+    return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt=""></img>
       <ul className='hidden md:flex item-start gap-5 font-medium'>
@@ -25,8 +29,27 @@ const Navbar = () => {
         </NavLink>
       </ul>
 
-      <div>
-        <button>Create Account</button>
+      <div className='flex item-center gap-4'>
+        {
+            token 
+            ? 
+            <div className="flex items-center gap-2 cursor-pointer group relative">
+  <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
+  <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+
+  
+  <div className="absolute top-0 right-0 pt-14 text-base font-small text-gray-400 z-20 hidden group-hover:block">
+    <p className="px-4 py-2 hover:bg-gray-100">My Profile</p>
+    <p className="px-4 py-2 hover:bg-gray-100">My Appointment</p>
+    <p className="px-4 py-2 hover:bg-gray-100">Logout</p>
+  </div>
+</div>
+
+             
+            : <button onClick={()=>navigate('/login')} className='bg-blue-500 text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            
+        }
+        
       </div>
     </div>
   )
