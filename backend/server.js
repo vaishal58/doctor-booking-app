d53b04092edf7230274d9c7d218b40edabbdde84
Vaishal shah <vaishal0508@gmail.com>
2025-09-22 17:33:09 -0400

    cloudinary and mongo setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+//app config
+const app=express();
+const port=process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+//middlewares
+app.use(express.json());
+app.use(cors())
+
+//api endpoint
+app.get('/',(req,res)=>{
+  res.send('api working')
+});
+
+app.listen(port,()=> console.log("server started",port));
+
