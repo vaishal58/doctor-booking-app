71bc23623e5ffa73640df65a0a00e2a124c47882
Vaishal shah <vaishal0508@gmail.com>
2025-09-28 18:00:55 -0400

    24th sep updtae

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,8 @@
 const addDoctor=async(req,res)=>{
   try{
     const {name,email,password,speciality,degree,experience,about,fees,address}=req.body;
+    const imageFile=req.file 
+    console.log({name,email,password,speciality,degree,experience,about,fees,address},imageFile);
     
   }
   catch(error)
@@ -9,3 +11,5 @@ const addDoctor=async(req,res)=>{
 
   }
 }
+
+export {addDoctor}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 //app config
 const app=express();
 const port=process.env.PORT || 4000
@@ -11,6 +12,8 @@ connectCloudinary()
 //middlewares
 app.use(express.json());
 app.use(cors())
+app.use('/api/admin',adminRouter)
+
 
 //api endpoint
 app.get('/',(req,res)=>{
